3d655ae77fc98d5d58622d8dfe52f1efdcea8e9c
lukexiao <814008000@163.com>
2023-09-10 11:25:52 +0800

    1

---
+++ src/lib/Peo.svelte
@@ -0,0 +1,9 @@
+<script>
+    export let name='kl';
+    export let age=18;
+</script>
+<main>
+    <h1>hello every</h1>
+    <p>my name is {name} and i am {age} years old</p>
+</main>
+<style></style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
+<script>
+  import Peo from "../lib/Peo.svelte";
+  import data from '$lib/peo.json'  
+
+</script>
+<Peo {...data}></Peo>
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
