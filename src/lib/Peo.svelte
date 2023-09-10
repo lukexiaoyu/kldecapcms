5a7977769f4994b88d8bc8d7720d08a507aa822e
lukexiao <814008000@163.com>
2023-09-10 11:28:21 +0800

    1

--- src/lib/Peo.svelte
+++ src/lib/Peo.svelte
@@ -1,9 +1,10 @@
 <script>
     export let name='kl';
     export let age=18;
+    export let sex='男';
 </script>
 <main>
     <h1>hello every</h1>
-    <p>my name is {name} and i am {age} years old</p>
+    <p>my name is {name} and i am {age} years old and i am a {sex} </p>
 </main>
 <style></style>
